9ad20802d77b642cb0acafb7c844ef667533cdf9
Ben <benwade37@me.com>
2026-05-29 12:19:09 +0100

    Secure True for media correction

--- config/settings.py
+++ config/settings.py
@@ -119,7 +119,7 @@ cloudinary.config(
     cloud_name=config('CLOUDINARY_CLOUD_NAME'),
     api_key=config('CLOUDINARY_API_KEY'),
     api_secret=config('CLOUDINARY_API_SECRET'),
-    secure=True;
+    secure=True,
 )
 
 STORAGES = {
