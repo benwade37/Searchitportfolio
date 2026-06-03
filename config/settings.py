27d60eaaeea48d3f1604b23fb71862c0a25430ad
Ben <benwade37@me.com>
2026-06-03 17:13:17 +0100

    Add clickawadey domain to Django settings

--- config/settings.py
+++ config/settings.py
@@ -36,10 +36,16 @@ SECRET_KEY = 'django-insecure-p1_%#n94u&5rcq9_rdvi!id^ji0$$riale8u17r0@fkk(i3544
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['searchitportfolio-a80afa0f6b57.herokuapp.com','localhost','127.0.0.1']
+ALLOWED_HOSTS = [
+'searchitportfolio-a80afa0f6b57.herokuapp.com',
+'www.clickawadey.com'
+'clickawadey.com',
+'localhost','127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = [
     "https://searchitportfolio-a80afa0f6b57.herokuapp.com",
+    "https://www.clickawadey.com",
+    "https://clickawadey.com",
 ]
 
 # Application definition
